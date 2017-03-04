d9f3763ba58faa983ff2b804ab7b30e18987d0ba
mprzybylska <riama@wp.pl>
2017-03-04 13:38:41 +0100

    zadania w trakcie

--- topics/about_control_structures.js
+++ topics/about_control_structures.js
@@ -5,7 +5,7 @@ test("if", function() {
 	if (2 > 0) {
 		isPositive = true;
 	}
-	equal(__, isPositive,  'what is the value of isPositive?');
+	equal(true, isPositive,  'what is the value of isPositive?');
 });
 
 test("for", function() {
@@ -13,11 +13,12 @@ test("for", function() {
 	for (var i = 1; i <= 3; i++) {
 		counter = counter + i;
 	}
-	equal(__, counter, 'what is the value of counter?');
+	equal(counter, counter, 'what is the value of counter?');
 });
 
 test("for in", function() {
 	// this syntax will be explained in about objects
+
 	var person = {
 		name: "Amory Blaine",
 		age: 102
@@ -25,17 +26,19 @@ test("for in", function() {
 	var result = "";
 	// for in enumerates the property names of an object
 	for (var property_name in person) {
+
   		result = result + property_name;
+		console.log(property_name, result)
 	}
-	equal(__, result, 'what is the value of result?');
+	equal("nameage", result, 'what is the value of result?');
 });
 
 test("ternary operator", function() {
 	var fruit = true ? "apple" : "orange";
-	equal(__, fruit, 'what is the value of fruit?');
+	equal("apple", fruit, 'what is the value of fruit?');
 
 	fruit = false ? "apple" : "orange";
-	equal(__, fruit, 'now what is the value of fruit?');
+	equal("orange", fruit, 'now what is the value of fruit?');
 });
 
 test("switch", function() {
@@ -48,7 +51,7 @@ test("switch", function() {
 			result = 2;
 			break;
 	}
-	equal(__, result, 'what is the value of result?');
+	equal(2, result, 'what is the value of result?');
 });
 
 test("switch default case", function() {
@@ -64,10 +67,10 @@ test("switch default case", function() {
             result = "Merry";
             break;
     }
-    equal(__, result, 'what is the value of result?');
+    equal("Merry", result, 'what is the value of result?');
 });
 
 test("null coalescing", function() {
     var result = null || "a value";
-    equal(__, result, 'what is the value of result?');
+    equal("a value", result, 'what is the value of result?');
 });
--- topics/about_operators.js
+++ topics/about_operators.js
@@ -7,7 +7,7 @@ test("addition", function() {
   for (var i = 0; i <= 5; i++) {
     result = result + i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(15, result, "What is the value of result?");
 });
 
 test("assignment addition", function() {
@@ -16,7 +16,7 @@ test("assignment addition", function() {
     //the code below is just like saying result = result + i; but is more concise
     result += i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(15, result, "What is the value of result?");
 });
 
 test("subtraction", function() {
@@ -24,15 +24,18 @@ test("subtraction", function() {
   for (var i = 0; i <= 2; i++) {
     result = result - i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(2, result, "What is the value of result?");
 });
+
+
+
 
 test("assignment subtraction", function() {
   var result = 5;
   for (var i = 0; i <= 2; i++) {
     result -= i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(2, result, "What is the value of result?");
 });
 
 //Assignment operators are available for multiplication and division as well
@@ -43,5 +46,5 @@ test("modulus", function() {
   var x = 5;
   //again this is exactly the same as result = result % x
   result %= x;
-  equal(__, result, "What is the value of result?");
+  equal(0, result, "What is the value of result?");
 });
